40338f49a2e9ccf81c8fbb6d6ee23afaf60f7378
Jawad Jahangir <jawadjahangir89@gmail.com>
2020-08-15 00:46:05 +0600

    posts index admin page created

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,32 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function index() {
+        $posts = Post::all();
+        return view('admin.posts.index', ['posts' => $posts]);
+    }
+
     public function show(Post $post) {
 
         return view('blog-post', ['post' => $post]);
+    }
+
+    public function create() {
+
+        return view('admin.posts.create');
+    }
+
+    public function store() {
+        $inputs = request()->validate([
+            'title' => 'required|min:8|max:255',
+            'body' => 'required',
+            'post_image' => 'file'
+        ]);
+
+        if (request('post_image')) {
+            $inputs['post_image'] = request('post_image')->store('images');
+        }
+        auth()->user()->posts()->create($inputs);
+        return back();
     }
 }
