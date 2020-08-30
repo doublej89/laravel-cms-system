89083ab3e8237467ae329d50c7bc64800a80388f
Jawad Jahangir <jawadjahangir89@gmail.com>
2020-08-30 07:56:53 +0600

    post title update length limitation removed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
 
     public function update(Post $post) {
         $inputs = request()->validate([
-            'title' => 'required|min:8|max:255',
+            'title' => 'required|max:255',
             'body' => 'required',
             'post_image' => 'file'
         ]);
